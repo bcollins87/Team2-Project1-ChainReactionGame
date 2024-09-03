01145d3d68b1e73d0f0ab365e7ebeccdbac9f95f
bcollins1987 <becollins@my.waketech.edu>
2024-09-03 12:28:07 -0400

    Final Update
    took player UI from the second level (it loads with the level from the original gamemanager. fixed the buttons on win screen to move to the next level. The restart button on the lose screen should reload the current scene.

--- Assets/Scripts/UIScripts/UIManager.cs
+++ Assets/Scripts/UIScripts/UIManager.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private Scene scene;
     public void OnClickQuitButton()
     {
         Application.Quit();
     }
     public void OnClickLevelOne()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
+        scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "MainMenu")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
+        }
+        else if (scene.name == "LevelOne")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LevelTwo");
+        }
+        else if(scene.name == "LevelTwo")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LevelOne");
+        }
     }
     public void OnClickMainMenu()
     {
@@ -31,5 +50,9 @@ public class UIManager : MonoBehaviour
     public void OnClickCreditsScene()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Credits");
+    }
+    public void OnClickRestartScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene.name);
     }
 }
